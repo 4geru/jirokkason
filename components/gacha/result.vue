24dfffd8844d4c93b53bbdac980297fa0d523295
大東祐太 <h-1456@H-1456-ohigashi-yuta.local>
2018-06-09 15:02:32 +0900

    gacha

---
+++ components/gacha/result.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <img class="vending" src="/ticket_vending_machine.jpg" />
+    <button @click="getItem()">購入する</button>
+
+    <div class="result">
+      <ul>
+        <li class="name">{{name}}</li>
+        <li class="menu">{{menu}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        name: 'hoge',
+        menu: 'fuga'
+      }
+    },
+    methods: {
+      getItem: () => {
+        self = this
+        self.name = 'foo'
+        self.menu = 'bar'
+
+      }
+    }
+  }
+</script>
+
+<style>
+.container {
+  padding: 10px;
+  text-align: center;
+}
+
+button {
+  background: rgba(255, 255, 0);
+}
+
+.vending {
+  margin: 30px;
+  width: 70%;
+}
+</style>
---
+++ pages/gacha/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        ラーメンを購入
+      </h1>
+    </div>
+
+    <result />
+  </section>
+</template>
+
+<script>
+import Result from '~/components/gacha/Result.vue'
+
+export default {
+  components: {
+    Result
+  }
+}
+</script>
